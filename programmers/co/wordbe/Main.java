28dd43e4c18aae9865d997d8f0bb51b61fef98ae
Wordbe <seonghojin3@gmail.com>
2021-03-19 02:25:37 +0900

    hash weejang

--- programmers/co/wordbe/Main.java
+++ programmers/co/wordbe/Main.java
@@ -1,7 +1,11 @@
 package co.wordbe;
 
+import co.wordbe.hash.Weejang;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello, world");
+
+        int solution = Weejang.solution(new String[][]{{"abc"}, {"bcc", "ca"}});
+        System.out.println(solution);
     }
 }
---
+++ programmers/co/wordbe/hash/Weejang.java
@@ -0,0 +1,32 @@
+package co.wordbe.hash;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Weejang {
+    public static int solution(String[][] clothes) {
+        int answer = 1;
+
+        HashMap<String, Integer> types = new HashMap<>();
+        for (int i=0; i<clothes.length; i++) {
+            String type = clothes[i][1];
+
+            Integer value = types.get(type);
+            if (value == null) {
+                types.put(type, 1);
+            } else {
+                types.put(type, value + 1);
+            }
+        }
+
+        Iterator<String> keys = types.keySet().iterator();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            Integer val = types.get(key);
+            answer = answer * (val + 1);
+        }
+
+        return answer - 1;
+    }
+}
